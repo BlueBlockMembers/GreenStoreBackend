0b66760a623cb5d447371edda9b7af620615e075
MadhushaPrasad <madushaprasad21@gmail.com>
2023-05-23 16:08:56 +0530

    fixed bugs

--- controller/SupermarketPrice.controller.js
+++ controller/SupermarketPrice.controller.js
@@ -1,66 +1,20 @@
 //Import supermarket price model
 const SupermarketPriceModel = require('./../model/product.model');
-
+let {add, update, deleteSupermarket, search, getAll} = require('./../service/SupermarketPrice.Service');
 const addSupermarketPrice = async (req, res) => {
-    const {itemId, itemName, yesterDayPrice, toDayPrice} = req.body;
-
-    const priceList = await SupermarketPriceModel.find();
-    const priceRecordCount = priceList.length;
-    const autoGenerateSuperMarketPriceID = `SUM${priceRecordCount + 1}`;
-
-    const newPrice = new SupermarketPriceModel({
-        superMarketPriceID: autoGenerateSuperMarketPriceID, itemId, itemName, yesterDayPrice, toDayPrice
-    })
-
-    newPrice.save().then((result) => {
-        res
-            .status(200)
-            .json({
-                message: "Supermarket Price added successfully", result: {
-                    data: result, response: true,
-                },
-            })
-    })
+    await add(req, res)
 }
 const updateSupermarketPrice = async (req, res) => {
-    const priceID = req.params.id
-    const {itemId, itemName, yesterDayPrice, toDayPrice} = req.body;
-
-    try {
-        const result = await SupermarketPriceModel.findOneAndUpdate({superMarketPriceID: priceID}, {
-            superMarketPriceID: priceID, itemId, itemName, yesterDayPrice, toDayPrice
-        });
-        res.status(200).send({message: "Supermarket Price updated successfully", data: result});
-    } catch (e) {
-        res.status(500).send({message: "Something went wrong", error: e});
-    }
+    await update(req, res);
 }
 const deleteSupermarketPrice = async (req, res) => {
-    const priceID = req.params.id
-
-    try {
-        const result = await SupermarketPriceModel.findOneAndDelete({superMarketPriceID: priceID});
-        res.status(200).send({message: "Supermarket Price deleted successfully", data: result});
-    } catch (e) {
-        res.status(500).send({message: "Something went wrong", error: e});
-    }
+    await deleteSupermarket(req, res);
 }
 const getAllSupermarketPrice = async (req, res) => {
-    try {
-        const result = await SupermarketPriceModel.find();
-        res.status(200).send({message: "All Super market price list", data: result});
-    } catch (e) {
-        res.status(500).send({message: "Something went wrong", error: e});
-    }
+    await getAll(req, res);
 }
 const getOneSupermarketPrice = async (req, res) => {
-    const {id} = req.params;
-    try {
-        const result = await SupermarketPriceModel.findOne({superMarketPriceID: id});
-        res.status(200).send({message: "Super market found", data: result});
-    } catch (e) {
-        res.status(500).send({message: "Something went wrong", error: e});
-    }
+    await search(req, res);
 }
 
 module.exports = {
--- model/SupermarketPrice.model.js
+++ model/SupermarketPrice.model.js
@@ -1,28 +1,21 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({
+const supermarketPriceSchema = mongoose.Schema({
     superMarketPriceID: {
-        type: String,
-        required: true,
-    },
-    itemId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-    },
-    itemName: {
-        type: String,
-        required: true
-    },
-    yesterDayPrice: {
-        type: String,
-        default: '0.00'
-    },
-    toDayPrice: {
-        type: String,
-        default: '0.00'
+        type: String, required: true,
+    }, itemId: {
+        type: mongoose.Schema.Types.ObjectId, required: true,
+    }, itemName: {
+        type: String, required: true
+    }, yesterDayPrice: {
+        type: String, default: '0.00'
+    }, toDayPrice: {
+        type: String, default: '0.00'
     },
 }, {
     timestamps: true,
 })
 
-exports.Product = mongoose.model('Product', productSchema);
+const SuperMarketPrice = mongoose.model('SupermarketPrice', supermarketPriceSchema);
+
+module.exports = SuperMarketPrice;
